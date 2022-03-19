0ff11bed8e5b2da6728793b8784310c5a6879c82
Nikita <nekytnp@gmail.com>
2022-03-19 18:09:55 +0200

    Added profile edit page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
 
   def user_avatar(user)
-    return image_tag(user.avatar.variant(:thumb), class: 'user_avatar rounded-circle border border-3') if user.avatar.attached?
-
-    content_tag :div, nil, class: 'user_avatar rounded-circle border border-3', style: "background: center / contain no-repeat url('#{asset_path('user.png')}')"
+    if user.avatar.attached?
+      image_tag(user.avatar, class: 'user_avatar rounded-circle border border-3')
+    else
+      content_tag :div, nil, class: 'user_avatar rounded-circle border border-3', style: "background: center / contain no-repeat url('#{asset_path('user.png')}')"
+    end
   end
 
 end
