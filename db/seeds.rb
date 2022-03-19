fa8e7d0280b7d07d124063a6bdb5cfdde73c3fc1
Nikita <nekytnp@gmail.com>
2022-03-19 18:27:46 +0200

    Added seeds.rb for creating first usr

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.delete_all
+Document.delete_all
+ActiveStorage::Attachment.all.each { |attachment| attachment.purge }
+
+# create admin
+User.create(
+  first_name: 'Main',
+  last_name: 'Admin',
+  email: 'admin@admin.com',
+  password: 'cmsadmin',
+  type: 'Admin'
+)
