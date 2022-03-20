d68ace7cb55c4230658105010b391dd31d237b97
Nikita <nekytnp@gmail.com>
2022-03-20 19:25:50 +0200

    Refactored user helpers, added some changes to editing accounts by admins.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,7 @@
 module UsersHelper
 
   def user_avatar(user)
-    if user.avatar.attached?
-      image_tag(user.avatar, class: 'user_avatar rounded-circle border border-3')
-    else
-      content_tag :div, nil, class: 'user_avatar rounded-circle border border-3', style: "background: center / contain no-repeat url('#{asset_path('user.png')}')"
-    end
+    content_tag :div, nil, class: 'user_avatar rounded-circle border border-3', style: "background: center / cover no-repeat url('#{avatar_url(user)}')"
   end
 
   def avatar_url(user)
