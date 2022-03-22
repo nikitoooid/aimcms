6b4c6b011b606a58635bf4616be791d866480616
Nikita <nekytnp@gmail.com>
2022-03-22 12:30:14 +0200

    Added processing images

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,14 @@ class User < ApplicationRecord
   def roles_available
     ROLES
   end
+
+  def avatar_url(variant=nil)
+    avatar_request = variant.nil? ? self.avatar : self.avatar.variant(resize_to_limit: variant)
+
+    if self.avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_path(avatar_request, only_path: true)
+    else
+      asset_path('user.png')
+    end
+  end
 end
