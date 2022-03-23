78058af764601d4cb04cf8d619cee0b07081842a
Nikita <nekytnp@gmail.com>
2022-03-23 20:09:33 +0200

    Page processing added

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
+  
   def index
+    @page = Page.find_by(slug: 'index') || Page.first
   end
+
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,29 @@
 class Page < ApplicationRecord
+  include ActionView::Helpers::TagHelper
+
+  def html_content
+    blocks = ActiveSupport::JSON.decode(self.content)['blocks']
+    result = []
+
+    blocks.each { |block| result.push create_block(block) }
+
+    result.join.to_s.html_safe
+  end
+
+  private
+
+  def create_block(b)
+    content = []
+
+    content.push b['content'] unless b['content'].nil?
+
+    unless b['blocks'].nil?
+      b['blocks'].each { |block| content.push create_block(block) }
+    end
+
+    content_tag(b['block'].to_sym, content.join.html_safe,
+                id: b['id'], class: b['classlist'], style: b['style'],
+                src: b['src'], href: b['href'], type: b['type'], data: b['data']) unless b['block'].nil?
+  end
+
 end
