40943500bb2e4cb395a7cab7537efdbd07e96604
Nikita <nekytnp@gmail.com>
2022-04-12 21:02:15 +0300

    Added settings routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :edit, :update, :destroy]
     resources :pages
     resources :blocks
+    resources :settings, only: [:index, :update]
     
   end
 
