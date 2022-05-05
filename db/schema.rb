8ca649e8cca6dacf65d51483f3415b676681de09
Nikita <nekytnp@gmail.com>
2022-05-05 20:39:48 +0300

    Meta fields added to form, localized

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_140925) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_05_170126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_140925) do
     t.json "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.string "og_title"
+    t.string "og_type"
+    t.string "og_image"
+    t.text "og_description"
+    t.string "og_url"
   end
 
   create_table "pages_styles", id: false, force: :cascade do |t|
