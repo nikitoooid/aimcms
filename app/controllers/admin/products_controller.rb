74120a6808e7cd3f569aa986fc6f88ae7b9e4a24
Nikita <nekytnp@gmail.com>
2022-06-23 18:40:25 +0300

    Product views added

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,36 +3,49 @@ class Admin::ProductsController < Admin::MainController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @products = Product.all
   end
 
   def show
-
   end
 
   def new
-    
+    @product = Product.new
   end
 
   def create
-    
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path, flash: { success: t('.success') }
+    else
+      flash[:danger] = t('.fail')
+      render :new
+    end
   end
 
   def update
-    
+    if @product.update(product_params)
+      redirect_to admin_products_path, flash: { success: t('.success') }
+    else
+      flash[:danger] = t('.fail')
+      render :new
+    end
   end
 
   def destroy
-    
+    @product.destroy
+
+    redirect_to admin_products_path, flash: { success: t('.success') }
   end
 
   private
 
   def set_product
-    
+    @product = Product.find(params[:id])
   end
 
   def product_params
-
+    params.require(:product).permit([:title, :slug, :product_category_id, :content])
   end
 end
