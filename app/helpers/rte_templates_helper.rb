1913f88e8d739078937d492893d018066c603b6e
Nikita <nekytnp@gmail.com>
2022-07-20 15:59:15 +0300

    rte_list helper can process children

--- app/helpers/rte_templates_helper.rb
+++ app/helpers/rte_templates_helper.rb
@@ -7,22 +7,29 @@ module RteTemplatesHelper
     
     model = Class.const_get(block['params']['model'])
 
-    model_objects = []
+    # model_objects = []
     
-    if block['params']['find'].nil? || block['params']['find']['key'].nil? || block['params']['find']['value'].nil?
-      model_objects = block['params']['order'].nil? ? model.all : model.order(block['params']['order'])
-    else
-      model_objects = model.where(block['params']['find']['key'] => block['params']['find']['value']) unless block['params']['find']['key'].nil? && block['params']['find']['value'].nil?
-    end
+    # if block['params']['find'].nil? || block['params']['find']['key'].nil? || block['params']['find']['value'].nil?
+    #   model_objects = block['params']['order'].nil? ? model.all : model.order(block['params']['order'])
+    # else
+    #   model_objects = model.where(block['params']['find']['key'] => block['params']['find']['value']) unless block['params']['find']['key'].nil? && block['params']['find']['value'].nil?
+    # end
 
-    model_objects = model_objects.limit(block['params']['limit'].to_i) unless block['params']['limit'].nil? && block['params']['limit'].to_i.zero?
-    result = []
+    # model_objects = model_objects.limit(block['params']['limit'].to_i) unless block['params']['limit'].nil? && block['params']['limit'].to_i.zero?
+    model_objects = get_objects_of(model, block['params'])
     
-    model_objects.each do |model_object|
-      result.push( create_block(block['blocks'].first, model_object) )
+    model_objects.each do |object|
+      model_objects += object.children unless object.children.nil? && object.children.empty?
     end
+    # result = []
+    
+    # model_objects.each do |model_object|
+    #   result.push( create_block(block['blocks'].first, model_object) )
+    # end
 
-    block['content'] = result.join
+    # block['content'] = result.join
+
+    block['content'] = get_html_blocks(model_objects, block['blocks'].first)
     block['content']
   end
 
@@ -57,6 +64,31 @@ module RteTemplatesHelper
   end
 
   private
+
+  def get_objects_of(model, params)
+    model_objects = []
+    
+    if params['find'].nil? || params['find']['key'].nil? || params['find']['value'].nil?
+      model_objects = params['order'].nil? ? model.all : model.order(params['order'])
+    else
+      model_objects = model.where(params['find']['key'] => params['find']['value']) unless params['find']['key'].nil? && params['find']['value'].nil?
+    end
+
+    model_objects = model_objects.limit(params['limit'].to_i) unless params['limit'].nil? && params['limit'].to_i.zero?
+    model_objects
+  end
+
+  def get_html_blocks(objects, template=nil)
+    return if template.nil?
+
+    result = []
+    
+    objects.each do |object|
+      result.push( create_block(template, object) )
+    end
+
+    result.join
+  end
 
   def block_invalid?(block)
     return true if block['blocks'].nil? || !block['blocks'].any? || block['params'].nil?
